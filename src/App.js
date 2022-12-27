e66b746fa0d67518a446d2dcc221dd3d89250749
Joshua Hunsche Jones <jhunschejones@github.com>
2022-12-27 13:11:10 -0600

    Add auth UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +20,19 @@ function App() {
           Learn React
         </a>
       </header>
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut} style={amplifyStyles.button}>Sign out</Button>
     </div>
   );
 }
 
-export default App;
+const amplifyStyles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  todo: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+}
+
+export default withAuthenticator(App);
